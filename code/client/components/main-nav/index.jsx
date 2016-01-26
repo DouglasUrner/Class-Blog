5d65a17dd4df7ca9c5f407a16878b44504a965e8
Douglas Urner <dlu@canishe.com>
2016-01-26 00:37:56 -0800

    Working on dynamic main nav.

--- code/client/components/main-nav/index.jsx
+++ code/client/components/main-nav/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import * as BS from "react-bootstrap";
+import {Link} from "react-router";
+import {LinkContainer} from "react-router-bootstrap";
 
-const ReactLoginButtons = BlazeToReact("loginButtons");
+const MainNavLoginButtons = BlazeToReact("loginButtons");
 
 const MainNav = () => (
   <div>
@@ -10,38 +12,72 @@ const MainNav = () => (
       <BS.Navbar.Header>
         <BS.Navbar.Toggle/>
         <BS.Navbar.Brand>
-          <a href="/"><b>MRHS</b> Photo Blog</a>
+          <Link to="/"><b>MRHS</b> Photo Blog</Link>
         </BS.Navbar.Brand>
       </BS.Navbar.Header>
 
       <BS.Navbar.Collapse>
 
         <BS.Nav>
+
           <BS.NavItem eventKey={1} href="#">Everyone</BS.NavItem>
           <BS.NavItem eventKey={2} href="#">Me</BS.NavItem>
-          <BS.NavDropdown eventKey={3} title="Exemplars" id="exemplar-dropdown">
-            <BS.MenuItem eventKey={3.1}  href="exemplars">Unit 1</BS.MenuItem>
-            <BS.MenuItem eventKey={3.2}>Unit 2</BS.MenuItem>
-            <BS.MenuItem eventKey={3.3}>Unit 3</BS.MenuItem>
-          </BS.NavDropdown>
-          <BS.NavDropdown eventKey={4} title="How To" id="howto-dropdown">
-            <BS.MenuItem eventKey={4.1} href="how-to">Create a blog post</BS.MenuItem>
-            <BS.MenuItem eventKey={4.2}>Add photographs to a post</BS.MenuItem>
-          </BS.NavDropdown>
-          <BS.NavDropdown eventKey={5} title="Admin" id="admin-dropdown">
-            <BS.MenuItem eventKey={5.1} href="users">Manage Students</BS.MenuItem>
-            <BS.MenuItem divider />
-            <BS.MenuItem eventKey={5.2}>Settings</BS.MenuItem>
-          </BS.NavDropdown>
+
+          <MainNavExemplars/>
+          <MainNavHowTos/>
+          <MainNavAdmin/>
+
         </BS.Nav>
 
         <BS.Nav pullRight>
-          <ReactLoginButtons/>
+          <MainNavLoginButtons/>
         </BS.Nav>
 
       </BS.Navbar.Collapse>
     </BS.Navbar>
   </div>
 );
+
+const MainNavExemplars = React.createClass({
+  // The Exemplars tab should be generated automatically if there are
+  // posts in the database that are tagged as exemplars. Then if there
+  // are posts tagged with units and a configurable threshold has been
+  // reached the tab should switch from a NavItem to a NavDropdown and
+  // items should be generated for each unit as a MenuItem.
+  render() {
+    return (
+      <BS.NavDropdown eventKey={3} title="Exemplars" id="exemplar-dropdown">
+        <BS.MenuItem eventKey={3.1}  href="exemplars">Unit 1</BS.MenuItem>
+        <BS.MenuItem eventKey={3.2}>Unit 2</BS.MenuItem>
+        <BS.MenuItem eventKey={3.3}>Unit 3</BS.MenuItem>
+      </BS.NavDropdown>
+    );
+  }
+});
+
+
+
+const MainNavAdmin = React.createClass({
+  render() {
+    if (true) {
+      return (
+        <BS.NavDropdown eventKey={5} title="Admin" id="admin-dropdown">
+          <LinkContainer to="users">
+            <BS.MenuItem eventKey={5.1}>Manage Students</BS.MenuItem>
+          </LinkContainer>
+          <LinkContainer to="#">
+            <BS.MenuItem eventKey={5.2}>Archive Posts</BS.MenuItem>
+          </LinkContainer>
+          <BS.MenuItem divider />
+          <LinkContainer to="#">
+            <BS.MenuItem eventKey={5.3}>Settings</BS.MenuItem>
+          </LinkContainer>
+        </BS.NavDropdown>
+      );
+    } else {
+      return null;
+    }
+  }
+});
 
 export default MainNav;
